aba1b854a7e47f673d8fc1b7c2d7b84893f3bc16
Wellington Feijo <p-wcosta@tce.sp.gov.br>
2024-12-05 09:36:53 -0300

    Adicionando elementos em um arrayList

---
+++ Utilizando-As-Collections/src/CollectionsBasic.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionsBasic {
+
+    public static void main(String[] args) {
+        //String[] nomes = {"Wellington","Joca","Manu"};
+        //ArrayList contem o tamnho 10 por padrão
+        List<String> nomesArrayList = new ArrayList<>(){{
+            add("1");
+            add("2");
+            add("3");
+        }};
+        System.out.println(nomesArrayList);
+       
+        //Get no ArrayList
+    }
+}
