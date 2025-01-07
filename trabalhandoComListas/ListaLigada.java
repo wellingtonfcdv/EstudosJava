f0d4462fe9b3dfff30f40027c46c95cbff6e0a29
Wellington Feijo <p-wcosta@tce.sp.gov.br>
2025-01-07 16:04:51 -0300

    Criação classe ListaLigada

---
+++ trabalhandoComListas/ListaLigada.java
@@ -0,0 +1,19 @@
+public class ListaLigada {
+    public void adicionaNoComeco(Object elemento){}
+
+    public void adiciona(Object elemento){}
+
+    public void adiciona(int posicao, Object elemento){}
+
+    public Object pega(int posicao){
+        return null;
+    }
+
+    public void remove(int posicao){
+
+    }
+
+    public int tamanho(){return 0;}
+
+    public boolean contem(Object o){return false;}
+}
