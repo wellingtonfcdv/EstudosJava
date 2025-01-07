b415ff27fb51c2f868be760b8f42006574e72bc7
Wellington Feijo <p-wcosta@tce.sp.gov.br>
2025-01-07 16:31:55 -0300

    Criação classe para teste

---
+++ trabalhandoComListas/TestaListaLigada.java
@@ -0,0 +1,13 @@
+public class TestaListaLigada {
+    public static void main(String[] args) {
+        ListaLigada lista = new ListaLigada();
+
+        System.out.println(lista);
+        lista.adicionaNoComeco("Wellington");
+        System.out.println(lista);
+        lista.adicionaNoComeco("Paulo");
+        System.out.println(lista);
+        lista.adicionaNoComeco("Guilherme");
+        System.out.println(lista);
+    }
+}
