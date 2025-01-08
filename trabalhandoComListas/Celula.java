5a8c8c9939a36c580e424d511f5810fc976e8595
Wellington Feijo <p-wcosta@tce.sp.gov.br>
2025-01-08 15:52:04 -0300

    Criação do parâmetro elemento e seu construtor

--- trabalhandoComListas/Celula.java
+++ trabalhandoComListas/Celula.java
@@ -1,10 +1,15 @@
 public class Celula {
     private Object elemento;
     private Celula proximo;
+    private Celula anterior;
 
     public Celula(Object elemento, Celula proximo) {
         this.elemento = elemento;
         this.proximo = proximo;
+    }
+
+    public Celula(Object elemento) {
+        this.elemento = elemento;
     }
 
     public Object getElemento() {
@@ -17,5 +22,13 @@ public class Celula {
 
     public Celula getProximo() {
         return proximo;
+    }
+
+    public Celula getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(Celula anterior) {
+        this.anterior = anterior;
     }
 }
