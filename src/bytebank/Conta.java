f60dfbb9cad94b08f9e57b06559142f198aee965
Wellington Feijo <p-wcosta@tce.sp.gov.br>
2025-04-09 11:53:28 -0300

    Criando o comportamento os Métodos

--- src/bytebank/Conta.java
+++ src/bytebank/Conta.java
@@ -1,9 +1,13 @@
 package bytebank;
 
-public class Conta {
+public class Conta { //Atributos
 	double saldo;
 	int agencia;
 	int numero;
 	String titular;
 	
+	//Comportamentos -> métodos
+	void deposita(double valor) {
+		
+	}
 }
