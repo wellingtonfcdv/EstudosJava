207ac256fd6b3f090b023c8458b031121426038b
Wellington Feijo <p-wcosta@tce.sp.gov.br>
2025-04-09 12:35:38 -0300

    Criando o método transfere

--- src/bytebank/Conta.java
+++ src/bytebank/Conta.java
@@ -19,4 +19,13 @@ public class Conta { //Atributos
 			return false;
 		}
 	}
+	
+	public boolean transfere(double valor, Conta destino) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		}
+		return false;
+	}
 }
--- src/bytebank/TestaMetodo.java
+++ src/bytebank/TestaMetodo.java
@@ -9,5 +9,17 @@ public class TestaMetodo {
 		
 		boolean conseguiuRetirar =  conta.saca(20);
 		System.out.println(conseguiuRetirar);
+		
+		Conta contaDaMarcela = new Conta();
+		contaDaMarcela.deposita(1000);
+		
+		boolean sucessoTransferencia = contaDaMarcela.transfere(2300, conta);
+		if(sucessoTransferencia) {
+			System.out.println("Transferência com sucesso.");
+		}else {
+			System.out.println("Sem saldo.");
+		}
+		System.out.println(contaDaMarcela.saldo);
+		System.out.println(conta.saldo);
 	}
 }
