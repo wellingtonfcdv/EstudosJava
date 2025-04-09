7c9595d144824df99009619fc74597956f10d32e
Wellington Feijo <p-wcosta@tce.sp.gov.br>
2025-04-09 10:00:41 -0300

    Instanciando classe e imprimindo valor

---
+++ src/bytebank/CriarConta.java
@@ -0,0 +1,9 @@
+package bytebank;
+
+public class CriarConta {
+	public static void main(String[] args) {
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 200;
+		System.out.println(primeiraConta.saldo);
+	}
+}
