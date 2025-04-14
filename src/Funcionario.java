8cec22bbe0e945b252db3768d6d3785fc5e64420
Wellington Feijo <p-wcosta@tce.sp.gov.br>
2025-04-14 16:59:14 -0300

    Trabalhando com classes abstratas. Ajustando repetição de código

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,32 +1,37 @@
 
-public abstract class Funcionario {//não pode instanciar dessa classe, porqye pe abstrata
+public abstract class Funcionario {// não pode instanciar dessa classe, porqye pe abstrata
+
+
 
 	private String nome;
 	private String cpf;
 	private double salario;
-	
-	//abstract no método significa que é um método sem corpo, não há implementação
+
+	// abstract no método significa que é um método sem corpo, não há implementação
 	public abstract double getBonificacao();
-	
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+
 	public double getSalario() {
 		return salario;
 	}
+
 	public void setSalario(double salario) {
 		this.salario = salario;
 	}
-	
-	
-	
+
 }
---
+++ src/TesteSistema.java
@@ -0,0 +1,19 @@
+
+public class TesteSistema {
+
+	public static void main(String[] args) {
+
+		Gerente g = new Gerente();
+		g.setSenha(222);
+		
+		Administrador adm = new Administrador();
+		adm.setSenha(2232);
+		
+		
+		SistemaInterno si = new SistemaInterno();
+		si.autentica(g);
+		si.autentica(adm);
+		
+	}
+
+}
