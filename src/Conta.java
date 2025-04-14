5628299314279fd96fd486cf1c0ccc18327bee71
Wellington Feijo <p-wcosta@tce.sp.gov.br>
2025-04-14 14:47:52 -0300

    Herança e construtores

---
+++ src/Conta.java
@@ -0,0 +1,89 @@
+
+public class Conta { //Atributos
+	private double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+	private static int total;
+	
+	
+	//construtor
+
+	public Conta(int agencia, int numero) {
+		Conta.total++;
+		System.out.println("Total de contas é " + total);
+		this.agencia = agencia;
+		this.numero = numero;
+		System.out.println("Estou criando uma nova conta..." + this.numero);
+	}
+	
+	
+	//Comportamentos -> métodos
+	void deposita(double valor) {
+		this.saldo = this.saldo + valor;
+	}
+	
+	
+
+	public boolean saca(double valor){
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public boolean transfere(double valor, Conta destino) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		}
+		return false;
+	}
+	
+	public double getSaldo() {
+		return this.saldo;
+	}
+	
+	public int getNumero() {
+		return this.numero;
+	}
+	
+	public void setNumero(int numero){//Encapsulamento
+		
+		if(numero <= 0) {
+			System.out.println("Não pode valor menor igual a 0");
+			return;
+		}
+		this.numero = numero;
+	}
+	
+	public int getAgencia() {
+		
+		return this.agencia;
+	}
+	
+	public void setAgencia(int agencia) {
+		if(agencia <= 0) {
+			System.out.println("Não pode valor menor igual a 0");
+			return;
+		}
+		
+		this.agencia = agencia;
+	}
+	
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	
+	public Cliente getTitular() {
+		return titular;
+	}
+	
+	public int getTotal() {
+		return Conta.total;
+	}
+}
+
