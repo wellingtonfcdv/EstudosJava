c8df1bdfde5be82694d97a08f9641bfc9ee69e85
Wellington Feijo <p-wcosta@tce.sp.gov.br>
2025-04-15 11:46:02 -0300

    Ajustando composição e interface

--- src/Gerente.java
+++ src/Gerente.java
@@ -2,7 +2,11 @@
 
 public class Gerente extends Funcionario implements Autenticavel{
 
-	private int senha;
+	private AutenticacaoUtil autenticador;
+	
+	public Gerente() {
+		this.autenticador = new AutenticacaoUtil();
+	}
 	
 	
 	public double getBonificacao() {
@@ -11,16 +15,14 @@ public class Gerente extends Funcionario implements Autenticavel{
 
 	@Override
 	public void setSenha(int senha) {
-		this.senha = senha;
+		this.autenticador.setSenha(senha);
+		
 	}
 
 	@Override
 	public boolean autentica(int senha) {
-		if(this.senha == senha) {
-			return true;
-		}else {
-			return false;
-		}
+		return this.autenticador.autentica(senha);
+		
 		
 	}
 	
